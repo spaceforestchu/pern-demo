6cdf2888fc47b51523781752c1ff57c09c42b876
Pak Chu <pakchu@MacBook-Pro.local>
2023-08-07 01:10:43 +0900

    eighth

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,10 +2,11 @@ const pgp = require("pg-promise")();
 require("dotenv").config();
 
 const cn = {
-  //host: process.env.PG_HOST,
-  //port: process.env.PG_PORT,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
-  //user: process.env.PG_USER,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
 };
 
 const db = pgp(cn);
